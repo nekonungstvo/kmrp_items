5a2636c31513861c7a1adf6596bf52ba94175994
xunto <hunterdelyx1@gmail.com>
2018-02-23 02:39:43 +0300

    Fix color of threads with spool

--- src/main/java/ru/konungstvo/kmrp_items/Spool.java
+++ src/main/java/ru/konungstvo/kmrp_items/Spool.java
@@ -3,59 +3,31 @@ package ru.konungstvo.kmrp_items;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
 
 import java.awt.*;
-import java.util.List;
 
 public class Spool extends Item {
-    private static int[] colors = {
-            Color.RED.getRGB(), Color.CYAN.getRGB(),
-            Color.GRAY.getRGB(), Color.YELLOW.getRGB(),
-    };
     private IIcon threadsLayer = null;
-
-    public Spool() {
-        super();
-        setHasSubtypes(true);
-    }
+    private static int DEFAULT_COLOR = Color.RED.getRGB();
 
     private int getColorFromNBT(ItemStack itemStack) {
         NBTTagCompound nbttagcompound = itemStack.getTagCompound();
 
         if (nbttagcompound == null) {
-            return colors[0];
+            return DEFAULT_COLOR;
         }
 
         NBTTagCompound displayTag = nbttagcompound.getCompoundTag("display");
 
         if (displayTag == null || !displayTag.hasKey("color")) {
-            return colors[0];
+            return DEFAULT_COLOR;
         }
 
         return displayTag.getInteger("color");
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void getSubItems(Item item, CreativeTabs creativeTabs, List subtypes) {
-        int i = 0;
-
-        for (int color : colors) {
-            NBTTagCompound rootTag = new NBTTagCompound();
-            NBTTagCompound displayTag = new NBTTagCompound();
-            rootTag.setTag("display", displayTag);
-            displayTag.setInteger("color", color);
-
-            ItemStack itemStack = new ItemStack(item, 1, i);
-            itemStack.setTagCompound(rootTag);
-            subtypes.add(itemStack);
-
-            i++;
-        }
     }
 
     @SideOnly(Side.CLIENT)
