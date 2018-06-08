0c750d675a7a31b6e2068d8866a68bd08baca7d8
xunto <hunterdelyx1@gmail.com>
2018-06-08 03:06:18 +0300

    Extend weapon from ItemSimple

--- src/main/java/ru/konungstvo/kmrp_items/common/item/ItemWeapon.java
+++ src/main/java/ru/konungstvo/kmrp_items/common/item/ItemWeapon.java
@@ -11,30 +11,10 @@ import net.minecraftforge.common.util.EnumHelper;
 import static ru.konungstvo.kmrp_items.common.CommonProxy.KMRPWeaponTab;
 
 
-public class ItemWeapon extends ItemSword {
-    private String name;
-
-    private static ToolMaterial NO_MATERIAL = EnumHelper.addToolMaterial(
-            "NO_MATERIAL", 0, 1000,
-            0.0F, 0.0F, 30
-    );
-
+public class ItemWeapon extends ItemSimple {
     public ItemWeapon(String name, String texture) {
-        super(NO_MATERIAL);
-        setUnlocalizedName(name);
+        super(name, texture);
         setCreativeTab(KMRPWeaponTab);
-        setTextureName(texture);
-
-        this.name = name;
-    }
-
-    public Item register() {
-        GameRegistry.registerItem(this, this.name);
-        return this;
-    }
-
-    @Override
-    public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_) {
-        return p_77659_1_;
+        setFull3D();
     }
 }
